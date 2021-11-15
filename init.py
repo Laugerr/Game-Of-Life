6fe3cfc3b2ace854a2eab0706c22a9e9f0490aa3
Laugerr <salaheddineelmanssourii@gmail.com>
2021-11-15 14:26:16 +0400

    Task1 - Updated

--- init.py
+++ init.py
@@ -1,11 +1,27 @@
-from gametools import Field
+from random import randint as rand
 
-def game_create_field():
-    input_w = int(input('Enter the width of the field: '))
-    input_h = int(input('Enter the hight of the field: '))
+def game_create_field() -> list[int]:
+    print(' ╔' + '═'*55 + '╗')
+    width = int(input('     ╔Enter the width of the field: '))
+    height = int(input('     ╚Enter the hight of the field: '))
+    print(' ╚' + '═'*55 + '╝')
 
-    game_field = Field(input_w,input_h)
+    while width <= 0 or height <= 0:
+        print('\n'*3 + " ▓!▓ Invalid dimensions inputs. Insert only Natural number ▓!▓\n " + "═"*61 + '\n'*3)
+        print(' ╔' + '═'*55 + '╗')
+        width = int(input('     ╔Enter the width of the field: '))
+        height = int(input('     ╚Enter the hight of the field: '))
+        print(' ╚' + '═'*55 + '╝')
 
-    game_field.draw_field()
+    return [width, height]
+        
+def game_init_field(width, height) -> list[list[int]]:
+        return [ [ 0 for w in range(width) ] for h in range(height) ]
 
-game_create_field()
+def game_print_field(game_field) -> list[list[int]]:
+    for h in range(len(game_field)):
+        print('\n'*5)
+        print('--------------Game Field--------------')
+        print(game_field[h])
+
+# game_create_field()
